e6318fdaeba00af514770618d1b90e42ea0ed608
Justin Starry <justin@starrystudio.com>
2024-07-17 22:52:22 +0200

    refactor: replace TransactionResults with TransactionCommitResult's

--- svm/src/lib.rs
+++ svm/src/lib.rs
@@ -11,6 +11,7 @@ pub mod program_loader;
 pub mod rollback_accounts;
 pub mod runtime_config;
 pub mod transaction_account_state_info;
+pub mod transaction_commit_result;
 pub mod transaction_error_metrics;
 pub mod transaction_processing_callback;
 pub mod transaction_processor;

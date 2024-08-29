12f88b8a471399aabc6bde7a569526363033eb38
Justin Starry <justin@anza.xyz>
2024-08-29 10:07:53 +0000

    add miri test

--- ci/test-miri.sh
+++ ci/test-miri.sh
@@ -11,7 +11,9 @@ _ cargo "+${rust_nightly}" miri test -p solana-program -- hash:: account_info::
 _ cargo "+${rust_nightly}" miri test -p solana-unified-scheduler-logic
 
 # test big endian branch
-_ cargo "+${rust_nightly}" miri test --target s390x-unknown-linux-gnu -p solana-program  -- "vote::state::tests::test_deserialize_vote_state" --exact
+_ cargo "+${rust_nightly}" miri test --target s390x-unknown-linux-gnu -p solana-program -- "vote::state::tests::test_deserialize_vote_state" --exact
+# test little endian branch for UB
+_ cargo "+${rust_nightly}" miri test -p solana-program -- "vote::state::tests::test_deserialize_vote_state" --exact
 
 # run intentionally-#[ignored] ub triggering tests for each to make sure they fail
 (! _ cargo "+${rust_nightly}" miri test -p solana-unified-scheduler-logic -- \

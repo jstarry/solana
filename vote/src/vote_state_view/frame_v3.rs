81c09ffe277904df9c5d1cf0ae72832a03ba66ee
Justin Starry <justin@anza.xyz>
2025-03-01 06:01:31 +0000

    store field frames

--- vote/src/vote_state_view/frame_v3.rs
+++ vote/src/vote_state_view/frame_v3.rs
@@ -14,10 +14,10 @@ use {
 #[derive(Debug, Clone)]
 #[cfg_attr(feature = "frozen-abi", derive(AbiExample))]
 pub(super) struct VoteStateFrameV3 {
-    pub(super) num_votes: u8,
-    pub(super) has_root_slot: bool,
-    pub(super) num_authorized_voters: u8,
-    pub(super) num_epoch_credits: u8,
+    pub(super) votes_frame: LandedVotesListFrame,
+    pub(super) root_slot_frame: RootSlotFrame,
+    pub(super) authorized_voters_frame: AuthorizedVotersListFrame,
+    pub(super) epoch_credits_frame: EpochCreditsListFrame,
 }
 
 impl VoteStateFrameV3 {
@@ -26,42 +26,24 @@ impl VoteStateFrameV3 {
         let mut cursor = std::io::Cursor::new(bytes);
         cursor.set_position(votes_offset as u64);
 
-        let votes = LandedVotesListFrame::read(&mut cursor)?;
-        let root_slot = RootSlotFrame::read(&mut cursor)?;
-        let authorized_voters = AuthorizedVotersListFrame::read(&mut cursor)?;
+        let votes_frame = LandedVotesListFrame::read(&mut cursor)?;
+        let root_slot_frame = RootSlotFrame::read(&mut cursor)?;
+        let authorized_voters_frame = AuthorizedVotersListFrame::read(&mut cursor)?;
         PriorVotersFrame::read(&mut cursor);
-        let epoch_credits = EpochCreditsListFrame::read(&mut cursor)?;
+        let epoch_credits_frame = EpochCreditsListFrame::read(&mut cursor)?;
         cursor.consume(core::mem::size_of::<BlockTimestamp>());
         // trailing bytes are allowed. consistent with default behavior of
         // function bincode::deserialize
         if cursor.position() as usize <= bytes.len() {
             Ok(Self {
-                num_votes: u8::try_from(votes.len()).map_err(|_| VoteStateViewError::ParseError)?,
-                has_root_slot: root_slot.has_root_slot(),
-                num_authorized_voters: u8::try_from(authorized_voters.len())
-                    .map_err(|_| VoteStateViewError::ParseError)?,
-                num_epoch_credits: u8::try_from(epoch_credits.len())
-                    .map_err(|_| VoteStateViewError::ParseError)?,
+                votes_frame,
+                root_slot_frame,
+                authorized_voters_frame,
+                epoch_credits_frame,
             })
         } else {
             Err(VoteStateViewError::ParseError)
         }
-    }
-
-    pub(super) fn votes_frame(&self) -> LandedVotesListFrame {
-        LandedVotesListFrame::new(self.num_votes as usize)
-    }
-
-    pub(super) fn root_slot_frame(&self) -> RootSlotFrame {
-        RootSlotFrame::new(self.has_root_slot)
-    }
-
-    pub(super) fn authorized_voters_frame(&self) -> AuthorizedVotersListFrame {
-        AuthorizedVotersListFrame::new(self.num_authorized_voters as usize)
-    }
-
-    pub(super) const fn epoch_credits_frame(&self) -> EpochCreditsListFrame {
-        EpochCreditsListFrame::new(self.num_epoch_credits as usize)
     }
 
     pub(super) fn get_field_offset(&self, field: Field) -> usize {
@@ -93,15 +75,15 @@ impl VoteStateFrameV3 {
     }
 
     fn root_slot_offset(&self) -> usize {
-        Self::votes_offset() + self.votes_frame().total_size()
+        Self::votes_offset() + self.votes_frame.total_size()
     }
 
     fn authorized_voters_offset(&self) -> usize {
-        self.root_slot_offset() + self.root_slot_frame().total_size()
+        self.root_slot_offset() + self.root_slot_frame.total_size()
     }
 
     fn prior_voters_offset(&self) -> usize {
-        self.authorized_voters_offset() + self.authorized_voters_frame().total_size()
+        self.authorized_voters_offset() + self.authorized_voters_frame.total_size()
     }
 
     fn epoch_credits_offset(&self) -> usize {
@@ -109,6 +91,6 @@ impl VoteStateFrameV3 {
     }
 
     fn last_timestamp_offset(&self) -> usize {
-        self.epoch_credits_offset() + self.epoch_credits_frame().total_size()
+        self.epoch_credits_offset() + self.epoch_credits_frame.total_size()
     }
 }

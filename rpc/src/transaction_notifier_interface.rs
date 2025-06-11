e0b6683f39c7729370807ac572c71073c4f08aec
Justin Starry <justin@anza.xyz>
2025-06-11 20:02:14 +0000

    geyser: update to ReplicaTransactionInfoV3

--- rpc/src/transaction_notifier_interface.rs
+++ rpc/src/transaction_notifier_interface.rs
@@ -1,6 +1,5 @@
 use {
-    solana_clock::Slot, solana_signature::Signature,
-    solana_transaction::sanitized::SanitizedTransaction,
+    solana_clock::Slot, solana_hash::Hash, solana_transaction::versioned::VersionedTransaction,
     solana_transaction_status::TransactionStatusMeta, std::sync::Arc,
 };
 
@@ -9,9 +8,10 @@ pub trait TransactionNotifier {
         &self,
         slot: Slot,
         transaction_slot_index: usize,
-        signature: &Signature,
+        message_hash: &Hash,
+        is_vote: bool,
         transaction_status_meta: &TransactionStatusMeta,
-        transaction: &SanitizedTransaction,
+        transaction: &VersionedTransaction,
     );
 }
 
